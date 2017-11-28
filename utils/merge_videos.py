428119cc4550bb24f15d5bdfafc3b20eb7480c7f
Guillaume Chanel <Guillaume.Chanel@unige.ch>
2017-11-28 18:51:45 +0100

    Add parser and tempdir management
    TODO: remains to manage tempdir in merge ffmpeg command

--- utils/merge_videos.py
+++ utils/merge_videos.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import os
 import logging
+import argparse
+import tempfile
 
 def cut_video(video_filename, time_filename, start_time, end_time, out_filename):
     # Load time file and select frames events only
@@ -21,22 +23,32 @@ def cut_video(video_filename, time_filename, start_time, end_time, out_filename)
     logging.info("file: {} - startFrame: {} - nbFrames: {} - startTime: {} - endTime {}".format(video_filename, startFrame, nbFrames, startVideoTime, endVideoTime))
     os.system('ffmpeg -y -i {input} -ss {start}  -to {stop} -c copy -fflags +genpts {out}'.format(start=startVideoTime, stop=endVideoTime, out=out_filename, input=video_filename))
 
-logging.basicConfig(level=logging.INFO)
 
-# This scripts takes two videos and merge then
-# This is done according to a start / end times for synchronysation
-video1_filename = '/home/chanel/data/EATMINT-2-bis/ExpeBackup/D02/D02_S01_video.mp4'
-time1_filename = '/home/chanel/data/EATMINT-2-bis/ExpeBackup/D02/D02_S01_video_times.csv'
-video2_filename = '/home/chanel/data/EATMINT-2-bis/ExpeBackup/D02/D02_S02_video.mp4'
-time2_filename = '/home/chanel/data/EATMINT-2-bis/ExpeBackup/D02/D02_S02_video_times.csv'
-out_filename = '/tmp/test.mkv'
-start_time = 1453480547
-end_time = 1453482347
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    parser = argparse.ArgumentParser(description='Cut and merge two videos according to start / end times')
+    parser.add_argument('video_files', type=str, nargs=2, help='video files to merge together, sound will be merged from the two only the video of the first is kept')
+    parser.add_argument('time_files', type=str, nargs=2, help='time files correponding to video_files')
+    parser.add_argument('start', type=float, help='start time of the merged video')
+    parser.add_argument('stop', type=float, help='stop time of the merged video')
+    args = parser.parse_args()
+
+    # Define tempory cutted files names
+    tmp_file = os.path.join(tempfile.gettempdir(), '{}.mkv')
+
+    # Now segment the videos
+    for i, v in enumerate(args.video_files):
+
+        cut_video(v, args.time_files[i], args.start, args.stop, tmp_file.format(i))
+
+    # Merge the videos
+    os.system('ffmpeg -y -i /tmp/1.mkv -i /tmp/2.mkv -map 0:0 -filter_complex "[0:1][1:1] amix" -c:v libx264 -preset ultrafast {}'.format(out_filename))
+
+    # Clean tempdir
+    for i, v in enumerate(args.video_files):
+        os.remove(tmp_file.format(i))
 
 
-# Now segment the videos
-cut_video(video1_filename, time1_filename, start_time, end_time, '/tmp/1.mkv')
-cut_video(video2_filename, time2_filename, start_time, end_time, '/tmp/2.mkv')
-
-# Merge the videos
-os.system('ffmpeg -y -i /tmp/1.mkv -i /tmp/2.mkv -map 0:0 -filter_complex "[0:1][1:1] amix" -c:v libx264 -preset ultrafast {}'.format(out_filename))
+if __name__ == '__main__':
+    main()
